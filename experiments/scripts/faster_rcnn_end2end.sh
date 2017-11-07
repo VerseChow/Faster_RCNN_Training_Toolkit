f9523f7e92e1cfe676bcc9238c68c7fd0b5a1642
VerseChow <ywchow@umich.edu>
2017-11-06 20:40:13 -0500

    MODIFIED: small fix for script

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -6,7 +6,7 @@
 # Example:
 # ./experiments/scripts/faster_rcnn_end2end.sh 0 VGG_CNN_M_1024 pascal_voc \
 #   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
-
+#${NET}_faster_rcnn_final.caffemodel
 set -x
 set -e
 
@@ -16,18 +16,18 @@ GPU_ID=$1
 NET=$2
 NET_lc=${NET,,}
 DATASET=$3
-
+TESTTRAIN=$4
 array=( $@ )
 len=${#array[@]}
-EXTRA_ARGS=${array[@]:3:$len}
+EXTRA_ARGS=${array[@]:5:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
 case $DATASET in
   progress)
     TRAIN_IMDB="progress_train"
-    TEST_IMDB="progess_test"
+    TEST_IMDB="progress_test"
     PT_DIR="progress"
-    ITERS=50000
+    ITERS=100000
   ;;  
   *)
     echo "No dataset given"
@@ -39,21 +39,27 @@ LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu ${GPU_ID} \
-  --solver models/${PT_DIR}/${NET}/faster_rcnn_end2end/solver.prototxt \
-  --weights data/faster_rcnn_models/${NET}_faster_rcnn_final.caffemodel \
-  --imdb ${TRAIN_IMDB} \
-  --iters ${ITERS} \
-  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-  ${EXTRA_ARGS}
+case $TESTTRAIN in
+  "-train")
+    time python2 ./tools/train_net.py --gpu ${GPU_ID} \
+      --solver models/${PT_DIR}/${NET}/faster_rcnn_end2end/solver.prototxt \
+      --weights data/faster_rcnn_models/vgg16_faster_rcnn_iter_karthik.caffemodel\
+      --imdb ${TRAIN_IMDB} \
+      --iters ${ITERS} \
+      --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+      ${EXTRA_ARGS}
+  ;;
+  "-test")
+    NET_DIR=$5
+    time python2 ./tools/test_net.py --gpu ${GPU_ID} \
+      --def models/${PT_DIR}/${NET}/faster_rcnn_end2end/test.prototxt \
+      --net ${NET_DIR} \
+      --imdb ${TEST_IMDB} \
+      --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+      ${EXTRA_ARGS}
+  ;;
+esac
 
 set +x
 NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
 set -x
-
-#time ./tools/test_net.py --gpu ${GPU_ID} \
-#  --def models/${PT_DIR}/${NET}/faster_rcnn_end2end/test.prototxt \
-#  --net ${NET_FINAL} \
-#  --imdb ${TEST_IMDB} \
-#  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-#  ${EXTRA_ARGS}
